4e24cdc6cf8d7bb93f9209e52afa596160589bd1
Adesunloro Oluwaseyi <amiltontech.co@gmail.com>
2022-08-17 10:04:21 +0100

    first commit

--- socail-media-dashboard/src/App.js
+++ socail-media-dashboard/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import "./index.css"
+import { UIData } from "./UIData"
+import { FaCaretDown, FaCaretUp, FaToggleOff, FaToggleOn } from "react-icons/fa"
+
 
 function App() {
+
+  const [isLight, setIsLight] = useState(false)
+
+  const ChangeMode = () => {
+    setIsLight(!isLight)
+  }
+
+
   return (
-    <div className="App">
+    <div className={`App ${isLight === false ? "AppLight" : ""}`}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Social Media Dashboard</h2>
+        <p>Total Followers: 23,004</p>
+        <hr />
+        <div className='toggle'>
+          <p>{isLight ? "Dark Mode" : "Light Mode"}</p>
+          {isLight ? <FaToggleOn
+            className='faToggle'
+            onClick={ChangeMode}
+          /> : <FaToggleOff
+            className='faToggle'
+            onClick={ChangeMode}
+          />}
+        </div>
       </header>
+
+      <div className='first'>
+        <div className='Cards'>
+          {UIData.first.map((item, index) => {
+            return (
+              <div key={index} className={`Card ${item.platform}`}>
+
+                <p className='name'><span className='Icon'>{item.platformIcon}</span> {item.name}</p>
+                <h2>{item.follower}</h2>
+                <p>{item.type}</p>
+                <p className={`${item.isGain ? "gain" : "loss"}`}>
+                  {item.isGain ? <FaCaretUp /> : <FaCaretDown />}{item.addition} Today</p>
+              </div>
+            )
+          })}
+
+        </div>
+        <div>
+          <h2 className='title'>Overview - Today</h2>
+        </div>
+        <div className='Cards'>
+          {UIData.second.map((item, index) => {
+            return (
+              <div className='Card' key={index}>
+                <div className='box'>
+                  <p>{item.type}</p>
+                  <p className={`Icon ${item.platform}`}>{item.platformIcon}</p>
+                </div>
+                <div className='box'>
+                  <h2>{item.count}</h2>
+                  <p
+                    className={`${item.isGain ? "gain" : "loss"}`}>
+                    {item.isGain ? <FaCaretUp /> : <FaCaretDown />}{item.addition}%
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ socail-media-dashboard/src/UIData.js
@@ -0,0 +1,109 @@
+import { FaTwitter, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
+
+export const UIData = {
+    first: [
+        {
+            name: "@nathanf",
+            follower: 1987,
+            platformIcon: <FaFacebook />,
+            platform: "Facebook",
+            type: "Followers",
+            addition: "12",
+            isGain: true
+        },
+        {
+            name: "@nathanf",
+            follower: 1044,
+            platformIcon: <FaTwitter />,
+            platform: "Twitter",
+            type: " Followers",
+            addition: "99 ",
+            isGain: true
+        },
+        {
+            name: "@realnathanf",
+            follower: 11000,
+            platformIcon: <FaInstagram />,
+            platform: "Instagram",
+            type: " Followers",
+            addition: "1099",
+            isGain: true
+        },
+        {
+            name: "Nathan F.",
+            follower: 8239,
+            platformIcon: <FaYoutube />,
+            platform: "Youtube",
+            type: " Subscribers",
+            addition: "144",
+            isGain: false
+        },
+
+    ],
+    second: [
+        {
+            type: " Page Views",
+            count: 87,
+            addition: 3,
+            platformIcon: <FaFacebook />,
+            isGain: true,
+            platform: "Facebook"
+        },
+        {
+            type: "Likes",
+            count: 52,
+            addition: 2,
+            platformIcon: <FaFacebook />,
+            isGain: false,
+            platform: "Facebook"
+        },
+        {
+            type: "Likes",
+            count: 5462,
+            addition: 2257,
+            platformIcon: <FaInstagram />,
+            isGain: true,
+            platform: "Instagram"
+        },
+        {
+            type: "Profile Views",
+            count: "52k",
+            addition: 1375,
+            platformIcon: <FaInstagram />,
+            isGain: true,
+            platform: "Instagram"
+        },
+        {
+            type: "Retweets",
+            count: 117,
+            addition: 303,
+            platformIcon: <FaTwitter />,
+            isGain: true,
+            platform: "Twitter"
+        },
+        {
+            type: "Likes",
+            count: 507,
+            addition: 553,
+            platformIcon: <FaTwitter />,
+            isGain: true,
+            platform: "Twitter"
+        },
+        {
+            type: "Likes",
+            count: 107,
+            addition: 19,
+            platformIcon: <FaYoutube />,
+            isGain: false,
+            platform: "Youtube"
+        },
+        {
+            type: "Total Views",
+            count: 1407,
+            addition: 12,
+            platformIcon: <FaYoutube />,
+            isGain: false,
+            platform: "Youtube"
+        },
+    ]
+}
